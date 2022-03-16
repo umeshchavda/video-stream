b457bb07a250e1c042dbd217faf9203fb5f611f8
levina <82658782+levina-lab@users.noreply.github.com>
2022-03-16 14:21:06 +0700

    [add] setup markup for inline command

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -1,9 +1,11 @@
-""" inline section button """
+""" inline markup section """
 
 
 from pyrogram.types import (
   InlineKeyboardButton,
   InlineKeyboardMarkup,
+  InlineQueryResultArticle,
+  InputTextMessageContent,
 )
 
 
@@ -54,5 +56,49 @@ back_mark = InlineKeyboardMarkup(
         "🔙 Go Back", callback_data="stream_menu_panel"
       )
     ]
+  ]
+)
+
+
+markup = []
+
+markup.extend(
+  [
+    InlineQueryResultArticle(
+      title="pause stream",
+      description="pause the current track being played.",
+      thumb_url="https://telegra.ph/file/6a9ae463fa10ceddac220.png",
+      input_message_content=InputTextMessageContent("/vpause"),
+    ),
+    InlineQueryResultArticle(
+      title="resume stream",
+      description="play the previously paused track.",
+      thumb_url="https://telegra.ph/file/a105311b41b12b8ef3072.png",
+      input_message_content=InputTextMessageContent("/vresume"),
+    ),
+    InlineQueryResultArticle(
+      title="mute stream",
+      description="mute the streamer userbot on group call.",
+      thumb_url="https://telegra.ph/file/b120099ed5e01850ba9bc.png",
+      input_message_content=InputTextMessageContent("/vmute"),
+    ),
+    InlineQueryResultArticle(
+      title="unmute stream",
+      description="unmute the streamer userbot on group call.",
+      thumb_url="https://telegra.ph/file/c9436ea693039422db3e4.png",
+      input_message_content=InputTextMessageContent("/vunmute"),
+    ),
+    InlineQueryResultArticle(
+      title="skip stream",
+      description="goes to the next track of song in queue.",
+      thumb_url="https://telegra.ph/file/2ea38e9b8c4ed6a654b24.png",
+      input_message_content=InputTextMessageContent("/vskip"),
+    ),
+    InlineQueryResultArticle(
+      title="stop stream",
+      description="stop playback of the track and clears the queue.",
+      thumb_url="https://telegra.ph/file/61e7d76a7d6ca76eceb77.png",
+      input_message_content=InputTextMessageContent("/vstop"),
+    ),
   ]
 )
